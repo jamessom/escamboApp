f1c6951f0e3209154e42e6ee78f7d86776267b2f
Jamessom Queiroz <jamessomqueiroz@gmail.com>
2017-08-29 00:50:45 -0300

    :notebook_with_decorative_cover: adicionando seeds

--- start.sh
+++ start.sh
@@ -2,8 +2,10 @@
 bundle check || bundle install
 #  Cria o banco de dados caso não exista
 bundle exec rails db:create
-#  Roda migrations se tiver
+#  Roda migrations
 bundle exec rails db:migrate
+#  Roda seeds
+bundle exec rails db:seed
 # Apaga a pasta temporaria
 rm tmp/* -Rf 
 #  Roda nosso servidor

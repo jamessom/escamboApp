a49242e27f3cad2f9a6b672a10ce69c87e24c720
Jamessom Queiroz <jamessomqueiroz@gmail.com>
2017-09-23 03:02:48 -0300

    :construction: fix: arquivos de teste e build

--- test/controllers/site/home_controller_test.rb
+++ test/controllers/site/home_controller_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
 class Site::HomeControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get site_home_index_url
+  test "should get path" do
+    get site_home_path
     assert_response :success
   end
 

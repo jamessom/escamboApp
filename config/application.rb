9830b88f4a9430fb999ede10f89dc491e25920c6
jamessom <jamessomqueiroz@gmail.com>
2021-01-15 01:49:53 -0300

    feat: configura generators removendo helper e setando assets engine

--- config/application.rb
+++ config/application.rb
@@ -7,5 +7,9 @@ Bundler.require(*Rails.groups)
 module App
   class Application < Rails::Application
     config.i18n.default_locale = :"pt-BR"
+
+    config.generators.helper false
+    config.generators.javascript_engine :js
+    config.generators.stylesheet_engine :scss
   end
 end

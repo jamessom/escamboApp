19a0c6b5d97c58767ced36909ff9cae834f728ac
jamessom <jamessomqueiroz@gmail.com>
2021-01-18 23:09:01 -0300

    adiciona formulario de criação de categoria

--- app/controllers/backoffice/categories_controller.rb
+++ app/controllers/backoffice/categories_controller.rb
@@ -3,11 +3,27 @@ class Backoffice::CategoriesController < BackofficeController
     @categories = Category.all
   end
 
-  def new; end
+  def new
+    @category = Category.new
+  end
 
-  def crete; end
+  def create
+    @category = Category.new(params_category)
+
+    unless @category.save
+      return render :new
+    end
+
+    redirect_to backoffice_categories_path, notice: "Categoria #{@category.description} salva com sucesso!"
+  end
 
   def edit; end
 
   def update; end
+
+  private
+
+  def params_category
+    params.require(:category).permit(:description)
+  end
 end

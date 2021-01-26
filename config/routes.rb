44ccbb24b8cfef544dc0797da6cf33ec8a512a96
jamessom <jamessomqueiroz@gmail.com>
2021-01-25 23:52:21 -0300

    correção dos links de nevegaçãõ

--- config/routes.rb
+++ config/routes.rb
@@ -8,14 +8,16 @@ Rails.application.routes.draw do
   root 'site/home#index'
   get 'backoffice', to: 'backoffice/dashboard#index'
 
+
   namespace :site do
     get 'home', to: 'home#index'
   end
 
   namespace :backoffice do
+    get 'backoffice', to: 'backoffice/dashboard#index'
     get 'dashboard', to: 'dashboard#index'
-    get 'admins', to: 'admins#index'
 
-    resources :categories, expect: [:show, :destroy]
+    resources :categories, except: [:show, :destroy]
+    resources :admins, except: [:show, :destroy]
   end
 end

9bb09149e99e881b56ebe6b076e481b60cb6f940
jamessom <jamessomqueiroz@gmail.com>
2021-01-27 23:43:30 -0300

    adiciona configurações do devise para rodar os testes

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,8 +3,16 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
-  # Add more helper methods to be used by all tests here...
+  include Devise::Test::IntegrationHelpers
+  include Warden::Test::Helpers
+
+  def log_in(user)
+    if integration_test?
+      login_as(user, :scope => :user)
+    end
+
+    sign_in(user)
+  end
 end
